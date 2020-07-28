00f800ac0d7ca08775438e9940b5ce6998e8f1ad
drspz <spz0707k@gmail.com>
2020-07-28 11:37:02 +0900

    実装のサーバーサイド部分

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
   
   def create
-    Post.create(content: params[:content])
+    post = Post.create(content: params[:content], checked: false)
     redirect_to action: :index
   end
 
